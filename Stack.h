ca3b014ec979e0b47bde4a0ddda4726a55d3ae1f
richardzhang121 <richard.zhang121@gmail.com>
2017-03-07 12:57:55 -0800

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,28 +1,14 @@
-#include "Stack.h"
 #include "Node.h"
-// stack class that creates stack commands
+//header file to show commands available
 using namespace std;
 
-Stack::Stack(){
-  head = NULL;
-}
-Stack::~Stack(){
-  Node* nextNode = head;
-  while (head){
-    nextNode = head->getNext();
-    delete head;
-    head = nextNode;
-  }
-}
-Node* Stack::pop(){
-  Node* toReturn = head;
-  head = toReturn->getNext();
-  return toReturn;
-}
-Node* Stack::peek(){
-  return head;
-}
-void Stack::push(Node* newNode){
-  newNode->setNext(head);
-  head = newNode;
-}
+class Stack{
+ public:
+  Stack();
+  Node* pop();
+  Node* peek();
+  void push(Node* newNode);
+  ~Stack();
+ private:
+  Node* head;
+};
